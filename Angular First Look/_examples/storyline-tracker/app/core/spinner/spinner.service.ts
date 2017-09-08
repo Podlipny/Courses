c07593522bbd6549d9fdb0ab69d97a0efc5196df
Podlipny <nick.47@seznam.cz>
2017-09-08 17:59:35 +0200

    angular firs look comments added

--- Angular First Look/_examples/storyline-tracker/app/core/spinner/spinner.service.ts
+++ Angular First Look/_examples/storyline-tracker/app/core/spinner/spinner.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Optional, SkipSelf } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+//An RxJS Subject is a special type of Observable that allows values to be multicasted to many Observers. 
+//While plain Observables are unicast (each subscribed Observer owns an independent execution of the Observable), Subjects are multicast.
 
 export interface SpinnerState {
   show: boolean;
@@ -17,7 +19,7 @@ export class SpinnerService {
   }
 
   show() {
-    this.spinnerSubject.next(<SpinnerState>{ show: true });
+    this.spinnerSubject.next(<SpinnerState>{ show: true }); //pomoci next posleme do Subject observable objektu novou value
   }
 
   hide() {
