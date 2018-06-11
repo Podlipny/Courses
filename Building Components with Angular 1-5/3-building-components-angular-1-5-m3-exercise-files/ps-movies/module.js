15f89971a75ecea7bdac0e8adcd6fbb5129a856f
Podlipny <nick.47@seznam.cz>
2018-06-11 21:50:52 +0200

    Building Components with Angular 1.5 added

---
+++ Building Components with Angular 1-5/3-building-components-angular-1-5-m3-exercise-files/ps-movies/module.js
@@ -0,0 +1,18 @@
+(function() {
+    "use strict";
+
+    /*  var module = angular.module("psMovies", ["ngRoute"]);
+    
+       module.config(function($routeProvider) {
+       
+       $routeProvider
+            .when("/list", { template: "<movie-list></movie-list>"})
+            .otherwise({ redirectTo: "/list"});
+        
+    });
+  */  
+
+    var module = angular.module("psMovies", ["ngComponentRouter", "ngAnimate"]);
+    module.value("$routerRootComponent", "movieApp");
+
+}());
---
+++ Building Components with Angular 1-5/3-building-components-angular-1-5-m3-exercise-files/ps-movies/movie-list.component.js
@@ -0,0 +1,54 @@
+(function() {
+    "use strict";
+
+    var module = angular.module("psMovies");
+
+    function fetchMovies($http) {
+        return $http.get("/movies.json")
+                    .then(function(response) {
+                        return response.data;
+                    });
+    }
+
+    function controller($http) {
+
+        var model = this;
+        model.movies = [];
+
+        model.$onInit = function() {
+            fetchMovies($http).then(function(movies) {
+                model.movies = movies;    
+            });
+        };
+        
+        model.upRating = function(movie) {
+            if(movie.rating < 5) {
+                movie.rating += 1;
+            }
+        };
+        
+        model.downRating = function(movie) {
+            if(movie.rating > 1) {
+                movie.rating -= 1;
+            }
+        };
+        
+        model.setRating = function(movie, rating) { 
+            movie.rating = rating;
+        };
+        
+        model.goTo = function(id) {
+            model.$router.navigate(["Details", {id:id}, "Overview"]);  
+        };
+    }
+
+    module.component("movieList", {
+        bindings: {
+            "$router": "<"
+        },
+        templateUrl: "/ps-movies/movie-list.component.html",
+        controllerAs: "model",
+        controller: ["$http", controller]
+    });
+
+} ());
