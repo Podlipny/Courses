1e526c80b24a6534fde2bd7b608a4b74520c9887
Podlipny <nick.47@seznam.cz>
2018-06-17 22:07:34 +0200

    Redux Saga modified

--- _React/Redux Saga/05/src/sagas/itemPriceSaga.js
+++ _React/Redux Saga/05/src/sagas/itemPriceSaga.js
@@ -12,12 +12,16 @@ function* fetchItemPrice(id,currency){
     const response = yield fetch(`http://localhost:8081/prices/${currency}/${id}`);
     const json = yield response.json();
     const price = json[0].price;
+    // updatujeme price v application state
     yield put(setItemPrice(id,price));
 }
 export function* itemPriceSaga(){
+    // musi byt splneny obe podminky v all
+    // vystup je ulozen v array
     const [{user},{items}] = yield all([
         take(SET_CURRENT_USER),
         take(SET_CART_ITEMS)
     ]);
+    // por kazdou item chceme ziskat jeji price
     yield items.map(item=>call(fetchItemPrice,item.id,user.country));
 }
